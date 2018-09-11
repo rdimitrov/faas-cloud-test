7ff840dc52cd406cb317d54272d393bea74d0862
Radoslav Dimitrov <rdimitrow@gmail.com>
2018-09-12 01:33:40 +0300

    Test a QR code reader function
    Signed-off-by: Radoslav Dimitrov <rdimitrow@gmail.com>

--- helloworld-py/handler.py
+++ helloworld-py/handler.py
@@ -1,7 +1,15 @@
+import qrtools
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-
-    return req
+    if req:
+        with open("/tmp/qrcode.png", "wb") as f:
+            f.write(req)
+        qr = qrtools.QR()
+        qr.decode("/tmp/qrcode.png")
+        return "Your QR says:", qr.data
+    else:
+        return "No QR image sent :)"
