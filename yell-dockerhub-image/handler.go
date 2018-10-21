59a41bfc7586742daee123e456e306bb05cfa7c0
Radoslav Dimitrov <rdimitrow@gmail.com>
2018-10-22 01:05:00 +0300

    Incremental change
    Signed-off-by: Radoslav Dimitrov <rdimitrow@gmail.com>

--- yell-dockerhub-image/handler.go
+++ yell-dockerhub-image/handler.go
@@ -3,21 +3,36 @@ package function
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Handle a serverless request
 func Handle(req []byte) string {
 
-	var mapreq map[string]interface{}
-	err := json.Unmarshal(req, &mapreq)
+	var mapRequest map[string]interface{}
+	err := json.Unmarshal(req, &mapRequest)
 
 	if err != nil {
 		return fmt.Sprintf("Missing or failed request. Error: %s\n", err)
 	}
 
-	pusher := mapreq["push_data"].(map[string]interface{})["pusher"]
-	repo_name := mapreq["repository"].(map[string]interface{})["repo_name"]
-	tag := mapreq["push_data"].(map[string]interface{})["tag"]
+	pusher := mapRequest["push_data"].(map[string]interface{})["pusher"]
+	repoName := mapRequest["repository"].(map[string]interface{})["repo_name"]
+	tag := mapRequest["push_data"].(map[string]interface{})["tag"]
 
-	return fmt.Sprintf("Thanks, %s! You successfully pushed to Docker Hub the following image - %s:%s\n", pusher, repo_name, tag)
+	return fmt.Sprintf("Thanks, %s! You successfully pushed to Docker Hub the following image - %s:%s! %s", pusher, repoName, tag, getEmoticons())
+}
+
+func getEmoticons() string {
+	var rStr string
+	var pool = []string{":openfaas:", ":whale:", ":thumbsup:", ":wave:", ":sunglasses:", ":ok_hand:", ":chart_with_upwards_trend:", ":sunrise:", ":smiley:", ":smiley_cat:", ":parrot:", ":rocket:", ":100:", ":muscle:", ":signal_strength:", ":man-cartwheeling:"}
+
+	rand.Seed(time.Now().UnixNano())
+	randomize := rand.Perm(len(pool))
+
+	for _, i := range randomize[:5] {
+		rStr = rStr + pool[i] + " "
+	}
+	return rStr
 }
