5ff1036615a405de95682997e8352a122e7795b6
Radoslav Dimitrov <rdimitrow@gmail.com>
2018-10-22 00:17:14 +0300

    Parse the JSON payload
    Signed-off-by: Radoslav Dimitrov <rdimitrow@gmail.com>

--- yell-dockerhub-image/handler.go
+++ yell-dockerhub-image/handler.go
@@ -1,10 +1,23 @@
 package function
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+
+	var mapreq map[string]interface{}
+	err := json.Unmarshal(req, &mapreq)
+
+	if err != nil {
+		return fmt.Sprintf("Missing or failed request. Error: %s\n", err)
+	}
+
+	pusher := mapreq["push_data"].(map[string]interface{})["pusher"]
+	repo_name := mapreq["repository"].(map[string]interface{})["repo_name"]
+	tag := mapreq["push_data"].(map[string]interface{})["tag"]
+
+	return fmt.Sprintf("Thanks, %s! You successfully pushed to Docker Hub the following image - %s:%s\n", pusher, repo_name, tag)
 }
